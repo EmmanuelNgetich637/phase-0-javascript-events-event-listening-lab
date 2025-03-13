0ea8547d81e86972966debd44b1366df776f25c6
Emmanuel Ngetich <engetich333@gmail.com>
2025-03-12 23:58:47 -0700

    creates javascript event

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Define the addingEventListener function
 function addingEventListener() {
+    // Select the DOM element to which the event listener will be added
+    const button = document.getElementById('button');
+
+    // Add an event listener to the button
+    button.addEventListener('click', function() {
+        // This function will be executed when the button is clicked
+        alert('Button was clicked!');
+    });
 }
+
+// Call the function to add the event listener
+addingEventListener();
